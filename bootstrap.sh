1de2908ab9b3028deb20fa25aaebf3ceb2121af5
Victor Yap <starlocke@gmail.com>
2022-01-16 15:38:20 -0500

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 cd mantisbt
 docker run --rm --interactive --tty --volume $(pwd):/app --user $(id -u):$(id -g) composer install --ignore-platform-reqs
+cd -
 cp ./mantisbt/config/config_inc.quick.php ./mantisbt/config/config_inc.php
 UUID=$(docker run --rm alpine cat /proc/sys/kernel/random/uuid)
 echo "\$g_crypto_master_salt     = '$UUID';" >> ./mantisbt/config/config_inc.php
-cd -
