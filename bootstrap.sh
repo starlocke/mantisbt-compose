b639046fbe6d2581ad2a589dd4597fe61fc506fe
Victor Yap <starlocke@gmail.com>
2022-01-16 15:52:13 -0500

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ cd mantisbt
 docker run --rm --interactive --tty --volume $(pwd):/app --user $(id -u):$(id -g) composer install --ignore-platform-reqs
 cd -
 cp ./mantisbt/config/config_inc.quick.php ./mantisbt/config/config_inc.php
-SALT=$(cat /dev/urandom | head -c 20 | base64)
+SALT=$(cat /dev/urandom | head -c 64 | base64 --wrap=0)
 echo "\$g_crypto_master_salt     = '$SALT';" >> ./mantisbt/config/config_inc.php
